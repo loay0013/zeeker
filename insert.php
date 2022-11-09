52b59d1bf0186af511b35489b89713d2a8b77b3d
loayhajjo <louy0013@edu.zealand.dk>
2022-11-09 20:37:02 +0100

    v1.2

--- insert.php
+++ insert.php
@@ -7,8 +7,9 @@ if(!empty($_POST["data"])) {
         move_uploaded_file($file["Img"]["tmp_name"], "uplods/" . basename($file["Img"]["name"]));
     }
 
-    $sql = "INSERT INTO zeeker (coupon_code,discount,Fast_discount,Date_discount,M_discount,B_discount, Uses_discount) values (:coupon_code,:discount,:Fast_discount,:Date_discount, :M_discount, :B_discount,:Uses_discount, :Slut_date)";
-    $bind = [":coupon_code" => $data["coupon_code"], ":discount" => $data["discount"], ":Fast_discount" => $data["Fast_discount"], ":Date_discount" => $data["Date_discount"], ":M_discount" => $data["M_discount"], ":B_discount" => $data["B_discount"], ":Uses_discount" => $data["Uses_discount"], ":Slut_date" => $data["Slut_date"]];
+
+    $sql = "INSERT INTO coupon (CouponCode, ProcentDiscount, FastDiscount, DateDiscount, MonthDiscount, BindDiscount, CvrDiscount, SpecifikDiscount, SlutDiscount) values(:CouponCode, :ProcentDiscount, :FastDiscount, :DateDiscount, :MonthDiscount, :BindDiscount, :CvrDiscount, :SpecifikDiscount, :SlutDiscount)";
+    $bind = [":CouponCode" => $data["CouponCode"], ":ProcentDiscount" => $data["ProcentDiscount"], ":FastDiscount" => $data["FastDiscount"], ":DateDiscount" => $data["DateDiscount"], ":MonthDiscount" => $data["MonthDiscount"], ":BindDiscount" => $data["BindDiscount"], ":CvrDiscount" => $data["CvrDiscount"], ":SpecifikDiscount" => $data["SpecifikDiscount"], ":SlutDiscount" => $data["SlutDiscount"]];
     $db->sql($sql, $bind, false);
 
     echo "<body style='font-size: 2rem; background-color: #000000; '></body>
@@ -54,22 +55,23 @@ if(!empty($_POST["data"])) {
 
 
 <form method="post" action="insert.php" enctype="multipart/form-data">
+
 	<div class="background col-12 d-flex justify-content-center align-items-center">
 		<div class="box d-flex flex-column justify-content-center px-4 py-5">
 			<div class="row justify-content-center m-0">
 				<div class="col-12">
 					<div class="form-group m-2 mt-0 mb-3">
 						<label for="coupon_code"> <p class="text-dark m-0 rubrik">Coupon code</p></label>
-						<input class="form-control" type="text" placeholder="Indtast coupon her" name="data[coupon_code]"  id="coupon_code" ></input>
+						<input class="form-control" type="text" placeholder="Indtast coupon her" name="data[CouponCode]"  id="CouponCode" >
 					</div>
 				</div>
 
 
 				<div class="col-12 col-md-6">
 					<div class="form-group m-2">
-						<label for="coupon_code"> <p class="text-dark m-0 rubrik">Antal gratis måneder</p></label>
+						<label for="MonthDiscount"> <p class="text-dark m-0 rubrik">Antal gratis måneder</p></label>
 						<div class="dropdown">
-                            <select class="dropdown dd-btn px-2" name="M_discount"  id="M_discount">
+                            <select class="dropdown dd-btn px-2" name="data[MonthDiscount]"  id="MonthDiscount">
                                 <option value="0">Antal gratis måneder</option>
                                 <option value="1">1</option>
                                 <option value="2">2</option>
@@ -85,15 +87,15 @@ if(!empty($_POST["data"])) {
 				<div class="col-12 col-md-6">
 					<div class="form-group m-2">
 						<div class="dropdown">
-                            <label for="coupon_code">
+                            <label for="BindDiscount">
                                 <p class="text-dark m-0 rubrik">Bindingsperiode</p></label>
-                            <select class="dropdown dd-btn px-2" name="B_discount"  id="B_discount">
-                                <option value="0">Bindingsperiode</option>
-                                <option value="3">3 måneder</option>
-								<option value="6">6 måneder</option>
-								<option value="9">9 måneder</option>
-								<option value="12">12 måneder</option>
-								<option value="12">Andet</option>
+                            <select name="data[BindDiscount]" id="BindDiscount" class="dropdown dd-btn px-2">
+                                <option  value="0">Bindingsperiode</option>
+                                <option  value="3">3 måneder</option>
+								<option  value="6">6 måneder</option>
+								<option  value="9">9 måneder</option>
+								<option  value="12">12 måneder</option>
+								<option  value="12">Andet</option>
 							</select>
 						</div>
 					</div>
@@ -101,17 +103,17 @@ if(!empty($_POST["data"])) {
 
 				<div class="col-12 col-md-6 mt-3">
 					<div class="form-group m-2 mb-4">
-						<label for="coupon_code"> <p class="text-dark m-0 rubrik">CVR-nummer</p></label>
-						<input class="form-control py-0" placeholder="Indtast CVR-nummer" type="text" name="data[Uses_discount]"  id="Uses_discount">
+						<label for="CvrDiscount"> <p class="text-dark m-0 rubrik">CVR-nummer</p></label>
+						<input class="form-control py-0" placeholder="Indtast CVR-nummer" type="text" name="data[CvrDiscount]" id="CvrDiscount">
 					</div>
 				</div>
 
 				<div class="col-12 col-md-6 mt-3">
 					<div class="form-group m-2 pb-2">
 						<div class="dropdown">
-							<label for="coupon_code">
+							<label for="SpecifikDiscount">
 								<p class="text-dark m-0 rubrik">Rabat coupon til specifik plan</p></label>
-							<select class="dropdown dd-btn px-2" name="B_discount"  id="B_discount">
+							<select class="dropdown dd-btn px-2" name="data[SpecifikDiscount]">
 								<option value="0">Specifik plan</option>
 								<option value="1">Premium</option>
 								<option value="2">Entreprise</option>
@@ -123,36 +125,36 @@ if(!empty($_POST["data"])) {
 
 				<div class="col-12 col-md-6 mt-5">
 					<div class="form-group m-2 mb-4">
-						<label for="coupon_code"> <p class="text-dark m-0 rubrik">Rabat pris</p></label>
-						<input class="form-control" placeholder="Indtast din pris her" type="text" name="data[Fast_discount]"  id="Fast_discount">
+						<label for="FastDiscount"> <p class="text-dark m-0 rubrik">Rabat pris</p></label>
+						<input class="form-control" placeholder="Indtast din pris her" type="number" name="data[FastDiscount]"  id="FastDiscount">
 					</div>
 				</div>
 
 				<div class="col-12 col-md-6 mt-5">
 					<div class="form-group m-2 mb-4">
 						<label for="coupon_code"> <p class="text-dark m-0 rubrik">Rabat procent</p></label>
-						<input class="form-control" placeholder="Indtast din procent her" type="text" name="data[discount]"  id="discount">
+						<input class="form-control" placeholder="Indtast din procent her" type="text" name="data[ProcentDiscount]"  id="ProcentDiscount">
 					</div>
 				</div>
 
 				<div class="col-12 col-md-6">
 					<div class="form-group m-2">
-						<label for="coupon_code"><p class="text-dark m-0 rubrik">Fra dato</p></label>
-						<input class="form-control " type="date" name="data[Date_discount]"  id="Date_discount">
+						<label for="DateDiscount"><p class="text-dark m-0 rubrik">Fra dato</p></label>
+						<input class="form-control " type="date" name="data[DateDiscount]"  id="DateDiscount">
 					</div>
 				</div>
 
 				<div class="col-12 col-md-6">
 					<div class="form-group m-2">
-						<label for="coupon_code"> <p class="text-dark m-0 rubrik">Til dato</p></label>
-						<input class="form-control" type="date" name="data[Slut_date]"  id="Slut_date">
+						<label for="SlutDiscount"> <p class="text-dark m-0 rubrik">Til dato</p></label>
+						<input class="form-control" type="date" name="data[SlutDiscount]"  id="SlutDiscount">
 					</div>
 				</div>
 		</div>
 			<div class="col-12 col-md-6 d-flex">
 				<div class="col-12 mt-5 d-flex justify-content-center">
 					<div class="d-flex justify-content-center align-items-center">
-						<button class="form-control btn btn-primary bg-gradient rounded-0 border-0" type="submit" id="btn-rabat">Oversigt</button>
+						<button class="form-control btn btn-primary bg-gradient rounded-0 border-0"  id="btn-rabat">Oversigt</button>
 					</div>
 				</div>
 				<div class="col-12 mt-5 d-flex justify-content-center">
